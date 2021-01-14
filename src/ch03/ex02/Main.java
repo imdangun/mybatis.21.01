883985eb56dbb414709ccef476dae7447945c50d
a <a@a.com>
2021-01-14 13:12:14 +0900

    a

---
+++ src/ch03/ex02/Main.java
@@ -0,0 +1,14 @@
+package ch03.ex02;
+
+import config.Configuration;
+
+public class Main {
+	public static void main(String[] args) {
+		Map mapper = Configuration.getMapper(Map.class);
+		
+		System.out.println(mapper.selectUsers());
+	}
+}
+/*
+[1 john 2025-05-05 서울시 마포구, 2 terry 2025-05-15 성남시 분당구]
+*/
---
+++ src/ch04/ex01/Main.java
@@ -0,0 +1,17 @@
+package ch04.ex01;
+
+import ch04.domain.User;
+import config.Configuration;
+
+public class Main {
+	public static void main(String[] args) {
+		Map mapper = Configuration.getMapper(Map.class);
+		
+		for(User user: mapper.selectUsers())
+			System.out.println(user);
+	}
+}
+/*
+1 john 2025-05-05 [사랑 너와 나의 연결고리, 정의 너와 나의 연대고리]
+2 terry 2025-05-15 [김치 네가 있어야 밥을 먹지, 비빔밥 동학농민항쟁이 만든 음식이다., 찹쌀떡 네가 그리워.]
+*/
